1c5bc4248310ef0b7a94df85bbcfb7fa113b0c71
jaredkeirns <59539968+keirnsj@users.noreply.github.com>
2022-02-06 13:25:23 -0500

    Formatting device compass similar to device location

--- lib/device_compass.dart
+++ lib/device_compass.dart
@@ -1,36 +1,32 @@
 import 'package:flutter_compass/flutter_compass.dart';
 import 'package:geolocator/geolocator.dart';
 
-class DeviceCompass {
-  /*Access the getDeviceHeading() function to get the compass bearing. The
-  heading will be returned type double. */
-  void getDeviceHeading() async {
-    LocationPermission permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.always ||
-        permission == LocationPermission.whileInUse) {
-      getHeading();
-    } else {
-      requestLocationPermission();
+Future<Object> determineHeading() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
     }
   }
 
-  requestLocationPermission() async {
-    LocationPermission permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.always ||
-        permission == LocationPermission.whileInUse) {
-      getHeading();
-    } else {
-      requestLocationPermission();
-    }
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
   }
-
-  getHeading() async {
-    final CompassEvent tmp = await FlutterCompass.events!.first;
-    double? heading = tmp.heading;
-    if (heading! < 0) {
-      heading = (360 - heading.abs());
-    }
-    double? ccHeading = (heading - 360).abs();
-    return ccHeading;
+  final CompassEvent tmp = await FlutterCompass.events!.first;
+  double? heading = tmp.heading;
+  if (heading! < 0) {
+    heading = (360 - heading.abs());
   }
+  double? ccHeading = (heading - 360).abs();
+  return ccHeading;
 }
